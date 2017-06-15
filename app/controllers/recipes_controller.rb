553642b049e5a99676237e1bc61999df22a291fb
bliferniz <vitalij.bliferniz@web.de>
2017-06-15 18:49:31 +0200

    fix default search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @search_param = params[:search_param] || 'mocha'
+    @search_param = params[:search_param] || 'chocolate'
     @recipes = Recipe.for @search_param
   end
 end
